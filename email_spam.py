e02b1fe6578612d245eb0eef4dbe4ca1e2f96fd3
Ananth <ananth.m@hotfoot.co.in>
2025-05-19 13:36:40 +0530

    mlmodel

---
+++ email_spam.py
@@ -0,0 +1,46 @@
+
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+df = pd.read_csv('C:\\Users\\AnanthM\\Documents\\spam_ml\\email_origin\\email_origin.csv')
+dff = pd.read_csv('C:\\Users\\AnanthM\\Documents\\spam_ml\\email_text\\email_text.csv')
+
+
+tfidf_vectorizer = TfidfVectorizer()
+label_encoder = LabelEncoder()
+df['origin'].fillna('', inplace=True)
+df['label'] = label_encoder.fit_transform(df['label'])  # e.g., Positive=2, Negative=0, Neutral=1
+df['origin'] = df['origin'].fillna('')
+
+X = tfidf_vectorizer.fit_transform(df['origin'])
+Y = label_encoder.fit_transform(df['label'])
+
+X_test = tfidf_vectorizer.transform(dff['text'])
+Y_test = label_encoder.transform(dff['label'])
+
+
+model = MultinomialNB()
+
+model.fit(X, Y)
+
+y_predict = model.predict(X_test)
+
+accuracy = accuracy_score(Y_test, y_predict)
+print(f"\nAccuracy Rate: {accuracy:.2f}")
+print("\nConfusion Matrix:")
+print(confusion_matrix(Y_test, y_predict))
+
+print(f"\nAccuracy: {accuracy_score(Y_test, y_predict):.2f}")
+
+#joblib.dump(model, 'C:/Users/AnanthM/Documents/New folder/model.joblib')
+#joblib.dump(tfidf_vectorizer, 'C:/Users/AnanthM/Documents/New folder/tfidf_vectorizer.joblib')
+#joblib.dump(label_encoder, 'C:/Users/AnanthM/Documents/New folder/label_encoder.joblib')
+
+
+
---
+++ server.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+import uvicorn
+import joblib
+import numpy as np
+#from email_spam import preprocess_text
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+model = joblib.load('C:\\Users\\AnanthM\\Documents\\spam_ml\\model.joblib')
+label_encoder = joblib.load('C:\\Users\\AnanthM\\Documents\\spam_ml\\label_encoder.joblib')
+tfidf_vectorizer = joblib.load('C:\\Users\\AnanthM\\Documents\\spam_ml\\tfidf_vectorizer.joblib')
+
+#tfidf_vectorizer = TfidfVectorizer()
+
+app = FastAPI()
+
+@app.get("/")
+
+def read_root():
+    return {"mesage": "Email Spam Detection API"}
+
+@app.post("/predict/")
+def predict_spam(msg: str):
+
+    email_vector = tfidf_vectorizer.transform([msg])
+
+    prediction = model.predict(email_vector)
+
+    label = "Spam" if prediction[0] == 1 else "Not Spam"
+
+    return {"prediction": label}
+
+msg = "All space usage is normal, and there are no slow-running queries. The database is also functioning normally"
+prediction = predict_spam(msg)
+print(prediction)
+
+if __name__ == "__main__":
+    uvicorn.run(app)
